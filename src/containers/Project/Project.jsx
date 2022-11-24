15338e4e59d73d30e9a84f94044ef5923060641b
prajwal-m <prajwalm42@gmail.com>
2022-11-24 14:18:57 +0530

    chore: add project section

--- src/containers/Project/Project.jsx
+++ src/containers/Project/Project.jsx
@@ -10,6 +10,64 @@ const Project = () => {
           title="Project"
           subtitle="Here you will find about about some of the Personal projects i worked on."
         />
+        <div className="project-wrapper">
+          <div className="project-info-container">
+            <img
+              className="project-image"
+              src="https://user-images.githubusercontent.com/46987044/203714942-63ecce7e-3482-4510-870b-b270acff09d2.jpg"
+              alt="recipe app"
+            />
+            <div>
+              <p className="project-title">Portfolio website</p>
+              <p className="project-info">
+                Portfolio website built using React.js
+              </p>
+              <div className="project-repo-link">
+                <a href="#test">
+                  <i class="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project-info-container">
+            <img
+              className="project-image"
+              src="https://user-images.githubusercontent.com/46987044/203715150-f6db0b61-6d31-44dc-a4e3-166a24368a38.png"
+              alt="recipe app"
+            />
+            <div>
+              <p className="project-title">Guess the number game</p>
+              <p className="project-info">
+                A number Guessing game built using React Native.
+              </p>
+              <div className="project-repo-link">
+                <a href="#test">
+                  <i class="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-info-container">
+            <img
+              className="project-image"
+              src="https://user-images.githubusercontent.com/46987044/203715156-4db1c778-f984-45c4-b275-f3a83bac0c8d.png"
+              alt="recipe app"
+            />
+            <div>
+              <p className="project-title">Recipe App</p>
+              <p className="project-info">
+                A recipe app for meals of different cusine built using React
+                Native.
+              </p>
+              <div className="project-repo-link">
+                <a href="#test">
+                  <i class="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
